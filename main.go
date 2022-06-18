6424bcf5571f2e03b116c93a85d7d7c47ed15c0f
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-06-19 02:06:44 +0900

    Refactoring

--- main.go
+++ main.go
@@ -3,46 +3,37 @@ package gocolor
 var _default string = "\u001b[0m"
 
 func Default(text string) string {
-	result := "\u001b[0m" + text
-	return result
+	return "\u001b[0m" + text
 }
 
 func Red(text string) string {
-	result := "\u001b[31m" + text + _default
-	return result
+	return "\u001b[31m" + text + _default
 }
 
 func Blue(text string) string {
-	result := "\u001b[34m" + text + _default
-	return result
+	return "\u001b[34m" + text + _default
 }
 
 func Yellow(text string) string {
-	result := "\u001b[33m" + text + _default
-	return result
+	return "\u001b[33m" + text + _default
 }
 
 func Green(text string) string {
-	result := "\u001b[32m" + text + _default
-	return result
+	return "\u001b[32m" + text + _default
 }
 
 func Purple(text string) string {
-	result := "\u001b[35m" + text + _default
-	return result
+	return "\u001b[35m" + text + _default
 }
 
 func Cyan(text string) string {
-	result := "\u001b[36m" + text + _default
-	return result
+	return "\u001b[36m" + text + _default
 }
 
 func White(text string) string {
-	result := "\u001b[37m" + text + _default
-	return result
+	return "\u001b[37m" + text + _default
 }
 
 func Black(text string) string {
-	result := "\u001b[30m" + text + _default
-	return result
+	return "\u001b[30m" + text + _default
 }
